46c4ce3d0b4f2ca2707b3f1cd9180bcaa7f8ccc0
Mike Mueller <mike@subfocal.net>
2018-11-30 22:28:32 -0800

    Day 1, improved.

--- src/advent/day01.rs
+++ src/advent/day01.rs
@@ -3,14 +3,19 @@ use failure::Error;
 use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use regex::Regex;
 
 #[derive(Default)]
 pub struct Solver;
 
 impl AdventSolver for Solver {
     fn solve(&mut self) -> Result<(), Error> {
-        let input_sequence = Self::read_input_sequence()?;
+        let input_sequence: Vec<isize> =
+            BufReader::new(File::open("input/day01.txt")?)
+                      .lines()
+                      .collect::<Result<Vec<String>, _>>()?
+                      .iter()
+                      .map(|s| s.parse::<isize>())
+                      .collect::<Result<Vec<isize>, _>>()?;
         Self::solve1(0, &input_sequence);
         Self::solve2(0, &input_sequence);
         Ok(())
@@ -18,49 +23,21 @@ impl AdventSolver for Solver {
 }
 
 impl Solver {
-    fn read_input_sequence() -> Result<Vec<isize>, Error> {
-        let file = BufReader::new(File::open("input/day01.txt")?);
-        let re = Regex::new(r"^(?P<sign>-|\+)(?P<value>\d+)$")?;
-        let mut result : Vec<isize> = Vec::new();
-        for line in file.lines() {
-            let line = line?;
-            let caps = match re.captures(&line) {
-                Some(caps) => caps,
-                None => return Err(format_err!("Didn't match regex: {}", line))
-            };
-            let sign = &caps["sign"];
-            let value = caps["value"].parse::<isize>()?;
-            result.push(
-                match sign {
-                   "-" => -value,
-                   "+" => value,
-                   _ => return Err(format_err!("Invalid sign: {}", sign))
-                }
-            );
-        }
-        Ok(result)
-    }
-
     fn solve1(initial_frequency: isize, input_sequence: &Vec<isize>) {
-        let mut freq = initial_frequency;
-        for input in input_sequence {
-            freq += input;
-        }
-        println!("Final Frequency: {}", freq);
+        println!("Final Frequency: {}",
+                 initial_frequency + input_sequence.iter().sum::<isize>());
     }
 
     fn solve2(initial_frequency: isize, input_sequence: &Vec<isize>) {
         let mut freq = initial_frequency;
-        let mut freqs_seen : HashSet<isize> = HashSet::new();
-        loop {
-            for input in input_sequence {
-                if freqs_seen.contains(&freq) {
-                    println!("First frequency seen twice: {}", freq);
-                    return;
-                }
-                freqs_seen.insert(freq);
-                freq += input;
+        let mut freqs_seen = HashSet::new();
+        for input in input_sequence.iter().cycle() {
+            freq += input;
+            if freqs_seen.contains(&freq) {
+                break;
             }
+            freqs_seen.insert(freq);
         }
+        println!("First frequency seen twice: {}", freq);
     }
 }
